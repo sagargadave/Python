b6d827b4af8b52757903167e82fa6f7a8d830d6a
Sagar Gadave <sagargadave1234@gmail.com>
2025-10-26 00:13:38 +0530

    add complex_numbers1.py

---
+++ complex_numbers1.py
@@ -0,0 +1,10 @@
+# Creating complex numbers
+c1 = complex(3, 4)  
+c2 = complex(5)     
+c3 = complex()      
+
+print(c1) #(3+4j)
+print(c2) #(5+0j)
+print(c3) #(0j)
+print(c1.real) #(3.0)
+print(c1.imag) #(4.0)
