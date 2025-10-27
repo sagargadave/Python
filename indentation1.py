839f30139141d5c761e7b7a5ec1ad769e05b3bf0
Sagar Gadave <sagargadave1234@gmail.com>
2025-10-27 14:43:50 +0530

    indentation1.py

---
+++ indentation1.py
@@ -0,0 +1,14 @@
+# Indentation In python
+# 1)In python indentation is used to define block of code.
+# 2)It tells the Python interpreter that a group of statements belongs to a specific block
+# 3)All statements with the same level of indentation are considered part of the same block
+# 4) Indentation is achieved using whitespace (spaces or tabs) at the beginning of each line.
+
+# for example : 
+
+if 10 > 2:
+    print("It's true")
+    print("In a indetation")
+else:
+    print("Its's false")
+print("not In a indetation")
